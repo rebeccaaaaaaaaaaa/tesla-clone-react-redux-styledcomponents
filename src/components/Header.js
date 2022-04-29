d5cdf8f6e48474837d1e580489c249df80e1a1f7
rebeccaaaaaaaaaaa <rebecca.ignacio@orramtech.com.br>
2022-04-28 22:18:01 -0300

    criando um map dos modelos dos carros usando redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,59 +1,128 @@
-import React, {useState} from 'react'
-import styled from 'styled-components'
-import {MdMenu} from 'react-icons/md'
-import {MdOutlineClose} from "react-icons/md"
-function Header() {
+import React, { useState } from "react";
+import styled from "styled-components";
+import { MdMenu } from "react-icons/md";
+import { MdOutlineClose } from "react-icons/md";
+import {selectCars} from '../features/car/carSlice';
+import {useSelector} from 'react-redux';
 
-    const [isOpen, setIsOpen] = useState(false)
+
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const cars = useSelector(selectCars);
 
   return (
     <Container>
-        <a>
-          <img
-            src="/images/logo.svg"
-          />
-        </a>
-        <Menu>
-           <a href=""> Modal S </a>
-           <a href=""> Modal 3 </a>
-           <a href=""> Modal x </a>
-           <a href=""> Modal Y </a>
-        </Menu>
-        <RightMenu>
-          <a href=""> Shop </a>
-          <a href=""> Tesla Account </a>
-          <CustomMenu onClick={() => setIsOpen(true)}/>
-        </RightMenu>
-        <BurgerNav show={isOpen}>
-          <CustomClose onClick={() => setIsOpen(false)}/>
-          <li><a title="Model S" href="/models" >Model S</a></li>
-          <li><a title="Model 3" href="/model3" >Model 3</a></li>
-          <li><a title="Model X" href="/modelx" >Model X</a></li>
-          <li><a title="Model Y" href="/modely" >Model Y</a></li>
-          <li><a title="Solar Roof" href="/solarroof" >Solar Roof</a></li>
-          <li><a title="Solar Panels" href="/solarpanels" >Solar Panels</a></li>
-          <li><a title="Existing Inventory" href="/inventory/new/m3" >Existing Inventory</a></li>
-          <li><a title="Used Inventory" href="/inventory/used/m3" >Used Inventory</a></li>
-          <li><a title="Trade-In" href="/tradein" >Trade-In</a></li>
-          <li><a title="Test Drive" href="/drive" >Test Drive</a></li>
-          <li><a title="Insurance" href="/insurance" >Insurance</a></li>
-          <li><a title="Powerwall" href="/powerwall" >Powerwall</a></li>
-          <li><a title="Commercial Energy" href="/commercial" >Commercial Energy</a></li>
-          <li><a title="Utilities" href="/utilities" >Utilities</a></li>
-          <li><a title="Charging" href="/charging" >Charging</a></li>
-          <li><a title="Find Us" href="/findus" >Find Us</a></li>
-          <li><a title="Support" href="/support" >Support</a></li>
-          <li><a title="Investor Relations" href="https://ir.tesla.com" >Investor Relations</a></li>
-          <li><a title="Shop" href="https://shop.tesla.com" >Shop</a></li>
-          <li><a title="Account" href="/teslaaccount" >Account</a></li>
-       
-        </BurgerNav>
-        
+      <a>
+        <img src="/images/logo.svg" />
+      </a>
+      <Menu>
+        {
+          cars && cars.map((car, index) => (
+            <a href="#" key={index}> {car} </a>
+          ))
+        }
+      </Menu>
+      <RightMenu>
+        <a href=""> Shop </a>
+        <a href=""> Tesla Account </a>
+        <CustomMenu onClick={() => setIsOpen(true)} />
+      </RightMenu>
+      <BurgerNav show={isOpen}>
+        <CustomClose onClick={() => setIsOpen(false)} />
+        {
+          cars && cars.map((car, index) => (
+           <li key={index}>
+              <a href="#"> {car} </a>
+           </li>
+          ))
+        }
+        <li>
+          <a title="Solar Roof" href="/solarroof">
+            Solar Roof
+          </a>
+        </li>
+        <li>
+          <a title="Solar Panels" href="/solarpanels">
+            Solar Panels
+          </a>
+        </li>
+        <li>
+          <a title="Existing Inventory" href="/inventory/new/m3">
+            Existing Inventory
+          </a>
+        </li>
+        <li>
+          <a title="Used Inventory" href="/inventory/used/m3">
+            Used Inventory
+          </a>
+        </li>
+        <li>
+          <a title="Trade-In" href="/tradein">
+            Trade-In
+          </a>
+        </li>
+        <li>
+          <a title="Test Drive" href="/drive">
+            Test Drive
+          </a>
+        </li>
+        <li>
+          <a title="Insurance" href="/insurance">
+            Insurance
+          </a>
+        </li>
+        <li>
+          <a title="Powerwall" href="/powerwall">
+            Powerwall
+          </a>
+        </li>
+        <li>
+          <a title="Commercial Energy" href="/commercial">
+            Commercial Energy
+          </a>
+        </li>
+        <li>
+          <a title="Utilities" href="/utilities">
+            Utilities
+          </a>
+        </li>
+        <li>
+          <a title="Charging" href="/charging">
+            Charging
+          </a>
+        </li>
+        <li>
+          <a title="Find Us" href="/findus">
+            Find Us
+          </a>
+        </li>
+        <li>
+          <a title="Support" href="/support">
+            Support
+          </a>
+        </li>
+        <li>
+          <a title="Investor Relations" href="https://ir.tesla.com">
+            Investor Relations
+          </a>
+        </li>
+        <li>
+          <a title="Shop" href="https://shop.tesla.com">
+            Shop
+          </a>
+        </li>
+        <li>
+          <a title="Account" href="/teslaaccount">
+            Account
+          </a>
+        </li>
+      </BurgerNav>
     </Container>
-  )
+  );
 }
 
-export default Header
+export default Header;
 
 const Container = styled.div`
   min-height: 60px;
@@ -65,16 +134,14 @@ const Container = styled.div`
   padding: 0 20px;
   align-items: center;
   justify-content: space-between;
-
- 
-`
+`;
 
 const Menu = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   flex: 1;
-  a{
+  a {
     font-weight: 600;
     text-transform: uppercase;
     padding: 0 10px;
@@ -83,28 +150,24 @@ const Menu = styled.div`
 
   @media (max-width: 768px) {
     display: none;
-
   }
-
-`
+`;
 
 const RightMenu = styled.div`
-
   display: flex;
   align-items: center;
 
-a{
-  font-weight: 600;
-  text-transform: uppercase;
-  margin-right :10px;
-  flex-wrap: nowrap;
-}
-`
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+    flex-wrap: nowrap;
+  }
+`;
 
 const CustomMenu = styled(MdMenu)`
   cursor: pointer;
-
-`
+`;
 
 const BurgerNav = styled.ul`
   position: fixed;
@@ -120,18 +183,17 @@ const BurgerNav = styled.ul`
   flex-direction: column;
   text-align: start;
   transition: transform 0.3s ease-in-out;
-  transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+  transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
 
-
-  li{
+  li {
     padding: 15px 0;
     border-bottom: 1px solid #e5e5e5;
 
-    a{
+    a {
       font-weight: 600;
     }
   }
-`
+`;
 
 const CustomClose = styled(MdOutlineClose)`
   position: absolute;
@@ -139,4 +201,4 @@ const CustomClose = styled(MdOutlineClose)`
   right: 20px;
   cursor: pointer;
   font-size: 25px;
-`
+`;
